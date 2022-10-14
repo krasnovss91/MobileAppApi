abd1367ab02e77b900c43a7dc6c083742bec79c1
krasnovss91 <krasnovstanislav91@gmail.com>
2022-10-14 09:16:41 +0400

    repository

--- src/main/java/com/repository/ApplicationRepository.java
+++ src/main/java/com/repository/ApplicationRepository.java
@@ -1,4 +1,9 @@
 package com.repository;
 
-public class ApplicationRepository {
+import com.model.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
 }
